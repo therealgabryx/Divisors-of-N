c5b73df0b95f85c0ee58862087ca31e50e171ca1
Gabriele Concli <gabriele.concli@gmail.com>
2020-05-14 09:05:29 +0200

    debug

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 function exec() {
-  const numInt = parseInt(prompt('Inserisci un numero..', '0'));
-  let divisori = []
-  for (let i = 1; i <= numInt; i++) {
-    if (numInt % i == 0) {
-      alert(`${numInt} e' divisibile per ${i}`);
-      divisori.push(i)
+  const num = parseInt(prompt('Inserisci un numero..', '0'));
+
+  if (!isNaN(num)) {
+    document.getElementById('num').innerHTML = `[ ${num} ]`;
+    var nums = [];
+
+    if (num > 1) {
+      for (let i = 1; i <= num; i++) {
+        if (num % i == 0) {
+          nums.push(i);
+        }
+      }
+    } else if (num == 1) {
+      nums.push(1);
+    } else {
+      nums = null;
     }
+
+    if (nums != null) {
+      document.getElementById('solution').innerHTML = `${num} e' divisibile per `;
+      for (let i = 0; i < nums.length; i++) {
+        if (i == nums.length - 1) {
+          document.getElementById('solution').innerHTML += `e ${nums[i]} `;
+        } else if (i == nums.length - 2) {
+          document.getElementById('solution').innerHTML += `${nums[i]} `;
+        } else {
+          document.getElementById('solution').innerHTML += `${nums[i]}, `;
+        }
+      }
+    } else {
+      document.getElementById('solution').innerHTML = 'non ha divisori';
+    }
+  } else {
+    document.getElementById('num').innerHTML = '?';
+    document.getElementById('solution').innerHTML = 'invalid input';
   }
-  document.write(`${numInt} e' divisibile per ${divisori}`)
 }
